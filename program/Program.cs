6cb7310c9e0bdb7e4a5c731eea24c76ecba95e53
Mikhail <m.kaminskiy@list.ru>
2022-08-11 15:30:49 +0700

    printed array

---
+++ program/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = {10,3,-7,5,7,2,1};
+
+printArray(numbers);
+
+int minIndex = 0;
+int maxIndex = 0;
+
+int min = numbers[0];
+int max = numbers[0];
+  
+void printArray(int[] array)
+{
+    for(int i = 0; i< array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
