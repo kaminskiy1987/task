b8a9709907e5648a03c6b73f43767e1b8bcffeac
Mikhail <m.kaminskiy@list.ru>
2022-08-11 15:39:59 +0700

    program

--- program/Program.cs
+++ program/Program.cs
@@ -7,7 +7,19 @@ int maxIndex = 0;
 
 int min = numbers[0];
 int max = numbers[0];
-  
+
+int i = 1;
+   
+while(i < numbers.Length)
+{
+    if (max < numbers[i])
+    {
+        max = numbers[i];
+        maxIndex = i;
+    }
+
+    i++
+}  
 void printArray(int[] array)
 {
     for(int i = 0; i< array.Length; i++)
