a24720f84f48673350b9ee15a89474d70cadf7de
Mikhail <m.kaminskiy@list.ru>
2022-09-06 18:05:42 +0700

    update program2

--- program2/Program.cs
+++ program2/Program.cs
@@ -1,10 +1,11 @@
 ﻿Console.Write("Введите массив чисел: ");
 
 string inputString = Console.ReadLine();
-string[] words = inputString.Split(' ', ',');
-string[] outputString = new string[inputString.Length] ;
 
-for(int i = 0; i < words.Length; i++ )
+string[] words = inputString.Split(' ', ',');
+string[] outputString = new string[inputString.Length];
+
+for(int i = 0; i < words.Length; i++)
 {
     if(words[i].Length <= 3)
     { 
@@ -12,14 +13,5 @@ for(int i = 0; i < words.Length; i++ )
     }
 }
 
-Console.WriteLine("[" + string.Join(" ",inputString) + "]");
-Console.WriteLine("[" + string.Join(" ",outputString) + "]");
-
-void printArray(string[] array)
-{
-	for (int i = 0; i < array.Length; i++)
-{
-	Console.Write( array[i] + " ");
-}
-//	Console.WriteLine();
-}
+Console.WriteLine("[" + string.Join(" ", inputString) + "]");
+Console.WriteLine("[" + string.Join(" ", outputString) + "]");
